e8d3fbd2eb4bfdca2f2ade17e37124adba964054
mfranceschi <mfranc.martin@gmail.com>
2021-12-04 02:10:22 +0000

    dunno if it's better

--- daolib/DAOLib.cpp
+++ daolib/DAOLib.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <functional>
 #include <stdexcept>
+#include <vector>
 
 // Thanks to https://zetcode.com/db/sqlitec/ for the helping code!
 
@@ -10,16 +11,28 @@ class ConnectedDatabase {
 private:
   sqlite3 *db;
   ConnectedDatabase(sqlite3 *db) : db(db) {}
+  friend class DatabaseConnectionFactory;
+
+  static int callbackHelper(void *modernCallbackPointer, int nbColumns,
+                            char **columnsContent, char **columnsName) {
+    const auto &callback =
+        *static_cast<ModernCallback_t *>(modernCallbackPointer);
+
+    std::vector<std::string> resultsAsVector(nbColumns);
+    for (int i = 0; i < nbColumns; i++) {
+      resultsAsVector[i] = columnsContent[i];
+    }
+
+    try {
+      callback(resultsAsVector);
+      return 0;
+    } catch (const std::exception &) {
+      return 1;
+    }
+  }
 
 public:
-  static ConnectedDatabase open() {
-    sqlite3 *db;
-    int rc = sqlite3_open(":memory:", &db);
-    if (rc == SQLITE_OK) {
-      return ConnectedDatabase(db);
-    } else
-      throw std::runtime_error("failed to open db!");
-  }
+  ~ConnectedDatabase() { sqlite3_close(db); }
 
   void runSimpleQuery(const std::string &query) {
     char *errMessage = nullptr;
@@ -29,28 +42,45 @@ public:
     }
   }
 
-  static int callbackHelper(void *modernCallbackPointer, int argc, char **argv,
-                            char **colName) {
-    const auto *callback =
-        static_cast<std::function<void(int, char **, char **)> *>(
-            modernCallbackPointer);
-    try {
-      (*callback)(argc, argv, colName);
-      return 0;
-    } catch (const std::exception &) {
-      return 1;
-    }
-  }
+  using ModernCallback_t =
+      std::function<void(const std::vector<std::string> &)>;
 
-  void
-  runWithCallback(const std::string &query,
-                  std::function<void(int, char **, char **)> modernCallback) {
+  void runWithCallback(const std::string &query,
+                       ModernCallback_t modernCallback) {
     char *errMessage = nullptr;
     int rc = sqlite3_exec(db, query.c_str(), callbackHelper, &modernCallback,
                           &errMessage);
     if (rc != SQLITE_OK) {
       throw std::runtime_error(errMessage);
     }
+  }
+};
+
+class DatabaseConnectionFactory {
+private:
+  static ConnectedDatabase open_internal(const char *filename, int flags) {
+    sqlite3 *db;
+    int rc = sqlite3_open_v2(filename, &db, flags, nullptr);
+    if (rc == SQLITE_OK) {
+      return ConnectedDatabase(db);
+    } else {
+      throw std::runtime_error("failed to open db!");
+    }
+  }
+
+public:
+  static ConnectedDatabase open() {
+    return open_internal(":memory:",
+                         SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
+  }
+
+  static ConnectedDatabase open(const std::string &filename) {
+    return open_internal(filename.c_str(), SQLITE_OPEN_READWRITE);
+  }
+
+  static ConnectedDatabase openOrCreate(const std::string &filename) {
+    return open_internal(filename.c_str(),
+                         SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
   }
 };
 
@@ -64,7 +94,7 @@ int callback_display_cars(void *_unused, int argc, char **argv,
 }
 
 void quickstart() {
-  ConnectedDatabase db = ConnectedDatabase::open();
+  ConnectedDatabase db = DatabaseConnectionFactory::open();
 
   std::string sql = "DROP TABLE IF EXISTS Cars;"
                     "CREATE TABLE Cars(Id INT, Name TEXT, Price INT);"
@@ -79,10 +109,11 @@ void quickstart() {
   db.runSimpleQuery(sql);
 
   std::string sql2 = "SELECT * FROM Cars";
-  db.runWithCallback(sql2, [](int argc, char **argv, char **colName) {
-    for (int i = 0; i < argc; i++) {
-      printf("%s = %s\n", colName[i], argv[i] ? argv[i] : "NULL");
+  db.runWithCallback(sql2, [](const std::vector<std::string> &row) {
+    for (const std::string &string : row) {
+      printf("- %s ", string.c_str());
     }
+    printf("\n");
   });
 }
 
