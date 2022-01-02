e70b8326a88b9af5f824c0df7824bbe54211cf2d
mfranceschi <mfranc.martin@gmail.com>
2022-01-02 21:03:21 +0100

    init tests infrastructure

--- cmake/generate_or_update_dao/function_generate_or_update_dao.cmake
+++ cmake/generate_or_update_dao/function_generate_or_update_dao.cmake
@@ -18,7 +18,7 @@ function(GENERATE_OR_UPDATE_DAO)
   # One C++ module per YAML file.
   # Re-generate C++ code if the YAML is newer.
   get_filename_component(YAML_FILE_WE ${THIS_ARGS_YAML_FILE} NAME_WE)
-  set(CPP_FILE_OF_CURRENT_YAML ${USER_DAO_LIB_OUTPUT_FOLDER}/${YAML_FILE_WE}.cpp)
+  set(CPP_FILE_OF_CURRENT_YAML ${USER_DAO_LIB_OUTPUT_FOLDER}/${YAML_FILE_WE}.hpp)
   if (${THIS_ARGS_YAML_FILE} IS_NEWER_THAN ${CPP_FILE_OF_CURRENT_YAML})
     execute_process(
       COMMAND Python3::Interpreter main_program.py --yaml_file=${THIS_ARGS_YAML_FILE} --output_folder=${USER_DAO_LIB_OUTPUT_FOLDER}
@@ -28,6 +28,6 @@ function(GENERATE_OR_UPDATE_DAO)
 
   # Last configs of the library.
   target_sources(${THIS_ARGS_USER_DAO_LIB_NAME} PRIVATE ${USER_DAO_LIB_CPP_FILES})
-  target_link_libraries(${THIS_ARGS_USER_DAO_LIB_NAME} PUBLIC DaoLib)
+  target_link_libraries(${THIS_ARGS_USER_DAO_LIB_NAME} PUBLIC xyz)
 
 endfunction()
