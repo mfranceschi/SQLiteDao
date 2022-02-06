457f7743647db9044a001bba7094599aacbfcdea
mfranceschi <mfranc.martin@gmail.com>
2022-02-06 02:55:06 +0000

    YESSSS

--- aaa.cpp
+++ aaa.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <functional>
 #include <iostream>
 #include <iterator>
@@ -11,18 +12,21 @@ using namespace std;
 template <typename T> struct Stream {
 public:
   using ElementProcessor_t = const std::function<void(const T &)> &;
-  using Predicate_t = const std::function<bool(const T &)> &;
+  using Predicate_t = std::function<bool(const T &)>;
 
-  virtual Stream<T> *filter(Predicate_t predicate) = 0;
+  virtual Stream<T> *
+  filter(const typename Stream<T>::Predicate_t &predicate) = 0;
 
   virtual vector<T> collectToVector() = 0;
 
   virtual void browse(ElementProcessor_t elementProcessor) = 0;
+
+  virtual ~Stream() = default;
 };
 
 template <typename T> struct StreamFromFilter : public Stream<T> {
 public:
-  Stream<T> *filter(typename Stream<T>::Predicate_t predicate) override {
+  Stream<T> *filter(const typename Stream<T>::Predicate_t &predicate) override {
     return new StreamFromFilter(this, predicate);
   }
 
@@ -32,15 +36,19 @@ public:
     return result;
   }
 
-  StreamFromFilter(Stream<T> *source, typename Stream<T>::Predicate_t predicate)
+  StreamFromFilter(Stream<T> *source,
+                   const typename Stream<T>::Predicate_t &predicate)
       : previous(source), predicate(predicate) {}
 
   void
   browse(typename Stream<T>::ElementProcessor_t elementProcessor) override {
-    const typename Stream<T>::ElementProcessor_t nestedElementProcessor =
-        [&](const T &element) {
-          if (predicate(element)) {
-            elementProcessor(element);
+    const auto &_pred = this->predicate;
+    const auto &_proc = elementProcessor;
+
+    typename Stream<T>::ElementProcessor_t nestedElementProcessor =
+        [&_pred, &_proc](const T &element) {
+          if (_pred(element)) {
+            _proc(element);
           }
         };
     previous->browse(nestedElementProcessor);
@@ -51,42 +59,34 @@ private:
   typename Stream<T>::Predicate_t predicate;
 };
 
-template <typename T, typename T_iterator_t>
+template <typename T, typename Collection_t>
 struct StreamFromCollection : public Stream<T> {
 public:
-  StreamFromCollection(const T_iterator_t &start, const T_iterator_t &end)
-      : sourceIterator(start), endSourceIterator(end) {}
+  StreamFromCollection(const Collection_t &collection)
+      : collection(collection) {}
 
-  Stream<T> *filter(typename Stream<T>::Predicate_t predicate) override {
+  Stream<T> *filter(const typename Stream<T>::Predicate_t &predicate) override {
     return new StreamFromFilter<T>(this, predicate);
   }
 
-  vector<T> collectToVector() override {
-    return vector<T>(sourceIterator, endSourceIterator);
-  }
+  vector<T> collectToVector() override { return vector<T>(collection); }
 
   void
   browse(typename Stream<T>::ElementProcessor_t elementProcessor) override {
-    std::remove_reference_t<std::remove_const_t<T_iterator_t>> browseIterator(
-        sourceIterator);
-    for (; browseIterator != endSourceIterator; browseIterator++) {
-      elementProcessor(*browseIterator);
+    for (const auto &element : collection) {
+      elementProcessor(element);
     }
   }
 
 private:
-  const T_iterator_t &sourceIterator;
-  const T_iterator_t &endSourceIterator;
+  const Collection_t &collection;
 };
 
 namespace Streams {
 template <typename T>
 Stream<T> *fromCollection(const std::vector<T> &sourceCollection) {
 
-  auto cbegin = sourceCollection.cbegin();
-  auto cend = sourceCollection.cend();
-
-  return new StreamFromCollection<T, decltype(cbegin)>(cbegin, cend);
+  return new StreamFromCollection<T, vector<T>>(sourceCollection);
 }
 } // namespace Streams
 
@@ -96,13 +96,22 @@ int main() {
   const auto lambdaIsPair = [](int input) { return input % 2 == 0; };
   const auto lambdaIsNonNull = [](int input) { return input != 0; };
 
-  vector<int> result = Streams::fromCollection(vecint)
-                           ->filter(lambdaIsPair)
-                           ->filter(lambdaIsNonNull)
-                           ->collectToVector();
+  Stream<int> *a = Streams::fromCollection(vecint);
+  assert(a->collectToVector().size() == 6);
+
+  Stream<int> *b = a->filter(lambdaIsPair);
+  assert(b->collectToVector().size() == 4);
+
+  Stream<int> *c = b->filter(lambdaIsNonNull);
+
+  vector<int> result = c->collectToVector();
   cout << "Result size = " << result.size() << endl;
 
   for (int v : result) {
     cout << v << ", " << endl;
   }
+
+  delete a;
+  delete b;
+  delete c;
 }
